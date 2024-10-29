84ef774b4c1b59e90b76be4a7997ffbb55b86ea8
Andries Mathenjwa <andriesmathenjwa52@gmail.com>
2024-10-29 17:50:13 +0200

    login Page ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,31 @@ import Link from "next/link";
 import { HOME_ROUTE, LOGIN_ROUTE, PROFILE_ROUTE, REGISTER_ROUTE } from "@/constants/routes";
 import "./header.scss";
 
-
 const Header = () => {
+    const isLogin = false;
+
     return (
         <header className="header">
             <nav className="nav">
-                <Link href={HOME_ROUTE}><div className="logo">taskList</div></Link>
+                <Link href={HOME_ROUTE}>
+                    <div className="logo">taskList</div>
+                </Link>
                 <ul className="nav-links">
-                    <Link href={LOGIN_ROUTE}><li>Login</li></Link>
-                    <Link href={REGISTER_ROUTE}><li>Register</li></Link>
-                    {/* <Link href={PROFILE_ROUTE}><li>Profile</li></Link> */}
+                    {isLogin ? (
+                        <>
+                            <Link href={PROFILE_ROUTE}><li>Profile</li></Link>
+                            <Link href={HOME_ROUTE}><li>Logout</li></Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link href={LOGIN_ROUTE}><li>Login</li></Link>
+                            <Link href={REGISTER_ROUTE}><li>Register</li></Link>
+                        </>
+                    )}
                 </ul>
             </nav>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
