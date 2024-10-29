0e751d0de5da5e167137532395dc7f6d48e9b4c3
Andries Mathenjwa <andriesmathenjwa52@gmail.com>
2024-10-29 14:33:45 +0200

    initial project with routing

---
+++ src/app/register/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./registerPage.scss"
+
+const RegisterPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default RegisterPage
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Link from "next/link";
+import { HOME_ROUTE, LOGIN_ROUTE, PROFILE_ROUTE, REGISTER_ROUTE } from "@/constants/routes";
+import "./header.scss";
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav className="nav">
+                <Link href={HOME_ROUTE}><div className="logo">taskList</div></Link>
+                <ul className="nav-links">
+                    <Link href={LOGIN_ROUTE}><li>Login</li></Link>
+                    <Link href={REGISTER_ROUTE}><li>Register</li></Link>
+                    {/* <Link href={PROFILE_ROUTE}><li>Profile</li></Link> */}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
