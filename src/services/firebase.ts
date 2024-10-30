9604c6b9a18f4c32681778087d0e2dab212e325e
Andries Matenjwa <andriesmathenjwa52@gmail.com>
2024-10-30 18:18:08 +0200

    security update

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,17 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCNJadd5xrlEDyxdTRBnXGAH4K95N-R5hk",
-  authDomain: "task-list-app-935cb.firebaseapp.com",
-  projectId: "task-list-app-935cb",
-  storageBucket: "task-list-app-935cb.appspot.com",
-  messagingSenderId: "291574937518",
-  appId: "1:291574937518:web:a828989976c47b37327a3f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
